a6b31d0cd45ea3d3b312fdfc27dd57ae992c5e51
Sylvain DENYSE <sylvain.denyse@gmail.com>
2025-03-09 13:45:56 +0100

    🛠️   configure Biome

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
+import tailwindcss from '@tailwindcss/vite'
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()]
-	}
-});
+  vite: {
+    plugins: [tailwindcss()],
+  },
+})
