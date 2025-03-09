ec5a919e82412c2ae707163e360ca2323728ce47
Sylvain DENYSE <sylvain.denyse@gmail.com>
2025-03-09 13:30:41 +0100

    🛠️   initial commit from Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config'
-
-import tailwind from '@astrojs/tailwind'
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-})
+	vite: {
+		plugins: [tailwindcss()]
+	}
+});
