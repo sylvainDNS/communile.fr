9893b7ad66e077071a194ac11f7f22f748d5ee8e
Sylvain DENYSE <sylvain.denyse@gmail.com>
2025-03-09 13:30:41 +0100

    🛠️   initial commit from Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-
-import cloudflare from '@astrojs/cloudflare'
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'server',
-  adapter: cloudflare(),
-})
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	output: "server",
+	adapter: cloudflare(),
+});
