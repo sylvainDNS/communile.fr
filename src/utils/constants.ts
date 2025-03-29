73a69c55a1713e33a55bd23c46b37eb59cbf0b36
Sylvain DENYSE <sylvain.denyse@gmail.com>
2025-03-29 15:09:17 +0100

    ✨   add footer component

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,14 @@
 export const SITE_NAME = 'Commun’Île'
+
+export const PATH = {
+  HOME: '/',
+  LE_WATTIGNIES: '/le-wattignies',
+  LES_LANDES_FERTILES: '/les-landes-fertiles',
+  LE_LABO_DIVA: '/le-labo-diva',
+  LE_BARILE: '/le-barile',
+  A_LA_CARTE_POSTALE: '/a-la-carte-postale',
+  QUI_SOMMES_NOUS: '/#qui-sommes-nous',
+  NOTRE_CONCEPT: '/#notre-concept',
+  EN_QUELLES_CHIFFRES: '/#en-quelques-chiffres',
+  SCIC: '/#scic',
+}
