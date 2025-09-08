07bffbfd96c6a7816417f9979170658a0d70f6a9
netoun <nicolas@lonestone.io>
2025-09-08 11:51:34 +0200

    ✨   add animejs dependency, create heading-grid component, and enhance various sections with new styles and layouts

---
+++ src/utils/dom.ts
@@ -0,0 +1,27 @@
+export const get = (selector: string, all?: boolean) =>
+  all ? document?.querySelectorAll(selector) : document?.querySelector(selector)
+
+export const on = (
+  selector: string | HTMLElement | Document,
+  event: string,
+  callback: (event: Event) => void,
+  all?: boolean
+) => {
+  if (all) {
+    const elements = document.querySelectorAll(selector as string)
+
+    elements?.forEach(element => {
+      element.addEventListener(event, callback)
+    })
+
+    return
+  }
+
+  if (typeof selector === 'string') {
+    ;(get(selector) as HTMLElement)?.addEventListener(event, callback)
+
+    return
+  }
+
+  selector?.addEventListener(event, callback)
+}
